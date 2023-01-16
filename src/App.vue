fbb63c350b6c7bd8d3e01ccb1b1392617a70a964
jdwcg <bigcloudjo@gmail.com>
2023-01-16 23:10:00 +0900

    ㅍ

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <nav class="menu">
+      <a href="" v-for="(navi, i) in navis" :key="i">{{ navi }}</a>
+    </nav>
+    <div class="list" v-for="(item, i) in items" :key="i">
+      <h3>{{ items[i] }}</h3>
+      <p>{{ prices[i] }}</p>
+      <button @click="report += 1">허위매물신고</button>
+      <span>신고수: {{ report }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      report: 0,
+      navis: ["Home", "shop", "about"],
+      items: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      prices: ["100", "200", "300"],
+    };
+  },
+  components: {},
+};
 </script>
 
-<style>
+<style lang="scss">
+.list {
+  padding: 1rem 0;
+}
+.menu {
+  background: darkslateblue;
+  padding: 1rem;
+  color: white;
+  border-radius: 10px;
+  a {
+    text-decoration: none;
+    color: white;
+    padding: 0 10px;
+  }
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  height: 100%;
+  background: #eee;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+.wrapper {
+  padding: 1rem;
+  background: white;
+  margin: 0 auto;
+  max-width: 480px;
+  height: 100%;
 }
 </style>
